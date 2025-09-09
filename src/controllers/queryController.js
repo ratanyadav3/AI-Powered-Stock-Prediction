9d512ce322dd7830d45147a4539a71f3f7588919
Ratan Yadav <rattuda2001@gmail.com>
2025-09-09 14:46:55 +0530

    Integrated NER module with Together AI and updated query controller

--- src/controllers/queryController.js
+++ src/controllers/queryController.js
@@ -1,16 +1,17 @@
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { extractEntities } from "../services/nerService.js";
 
 const Userquery = asyncHandler(async (req, res) => {
     const data = req.body;
-
+    const prompt = data.Query;
     if (!data || Object.keys(data).length === 0) {
         throw new ApiError(400, "Query body is required");
     }
-
+   const ExtractedData = await extractEntities(prompt);
     return res.status(201).json(
-        new ApiResponse(201, "Just Checking my Incoming body", data)
+        new ApiResponse(201, "Just Checking my Incoming body", ExtractedData)
     );
 });
 
